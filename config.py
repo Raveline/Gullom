00a901627d2f1096bdaeb60865675d0ed0b7ccbd
Raveline <eraveline@gmail.com>
2014-03-01 16:25:23 +0100

    Config template.

---
+++ config.py
@@ -0,0 +1,7 @@
+USERNAME = ""   # your username
+PASSWORD = ""   # your password (cleartexted... I plan to hash this, though, because it's ugly)
+SECRET_KEY = "" # add a secret key (smashing your keyboard furiously can do)
+ROOT = ""       # The absolute path of your app. (Used because saving image with relative
+                # path was a bit of a pain).
+DATABASE = ""   # Absolute path to the DB. So if it's a "giffy.db" file in your app folder,
+                # you can do a ROOT + "giffy.db".
